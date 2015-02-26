552efdcfaa2c9b4eadc98972704ce0b0608a0542
PirateTigo <piratetigo@gmail.com>
2015-02-26 22:06:14 +0600

    Task3: scene was added, maked materials and static gameobjects

---
+++ Task3/Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Awake() {
+
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
